c8f780cdc3ef21eba9b7d42230eb239da914f350
YevgenyDyachenko <yevgeny.dyachenko@gmail.com>
2023-03-04 16:52:19 +0200

    new ip 3.23.182.238

--- frontend/src/components/todo/AddTodo.js
+++ frontend/src/components/todo/AddTodo.js
@@ -19,7 +19,7 @@ function AddTodo({isAuthenticated, setIsAuthenticated}) {
     e.preventDefault();
 
     try {
-      await axios.post('http://3.135.246.60:3001/api/todo', {title, targetDate}, {
+      await axios.post('http://3.23.182.238:3001/api/todo', {title, targetDate}, {
         headers: {
           'Authorization': `Bearer ${sessionStorage.getItem('token')}`,
         }
